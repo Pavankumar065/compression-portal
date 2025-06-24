18c9f7f7cc502d9511eb399bf760f2821aea5fb8
Pavankumar065 <ponnagantipavankumar13@gmail.com>
2025-06-25 03:20:24 +0530

    Update lz77.js

--- backend/algorithms/lz77.js
+++ backend/algorithms/lz77.js
@@ -1,15 +1,1 @@
 
-
-function lz77Compress(buffer) {
-   
-    console.log('LZ77 Compression - This is a placeholder. Full implementation required.');
-    return Buffer.from('lz77_compressed_data_placeholder');
-}
-
-function lz77Decompress(compressedBuffer) {
-  
-    console.log('LZ77 Decompression - This is a placeholder. Full implementation required.');
-    return Buffer.from('lz77_decompressed_data_placeholder');
-}
-
-module.exports = { lz77Compress, lz77Decompress };
